2011193d24e44dbbeb3cc2799049ba905326b9b4
chenguocan <2622870670@qq.com>
2020-10-30 10:22:27 +0800

    添加组件

---
+++ Component/NavHeader/NavHeader.js
@@ -0,0 +1,30 @@
+// Component/NavHeader/NavHeader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+      title:{
+        type:String,
+        value:'我是标题'
+      },
+      nav:{
+        type:String,
+        value:'我是导航',
+      }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
